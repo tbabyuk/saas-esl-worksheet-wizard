e3ac2bb0a94e0cf6ea4a54cfe23bc36f7503a592
Terry Babyuk <tbabyuk@gmail.com>
2024-05-31 17:50:21 -0400

    attempt 2 - removed imports

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,5 @@
 import Image from "next/image";
 
-import { HeaderSection } from "./home-components/Header";
-import { PricingSection } from "./home-components/Pricing";
 
 export default function Home() {
   return (
