72f27f73aa7750a839d0d7a3cbca686ff2a5641c
Terry Babyuk <tbabyuk@gmail.com>
2024-06-12 15:03:23 -0400

    testing Sidebar and Navbar component name change

---
+++ app/components/Topnav.jsx
@@ -0,0 +1,31 @@
+import { GiHamburgerMenu } from "react-icons/gi";
+import { RxHamburgerMenu } from "react-icons/rx";
+import { Sidebar } from "./Sidebar";
+import { SignOutButton } from "@clerk/nextjs";
+import { UserButton } from "@clerk/nextjs";
+
+
+export const Topnav = () => {
+
+  return (
+      <div className="h-[45px] bg-wizard-dark-blue">
+        <div className="drawer">
+          <input id="my-drawer" type="checkbox" className="drawer-toggle" />
+          <div className="drawer-content text-white relative">
+            {/* Page content here */}
+            <label htmlFor="my-drawer" className="btn btn-ghost drawer-button md:hidden">
+              <RxHamburgerMenu size="1.6rem" className="text-gray-100" />
+            </label>
+            <div className="absolute top-2.5 right-3"><UserButton afterSignOutUrl="/"  /></div>
+          </div> 
+          <div className="drawer-side">
+            <label htmlFor="my-drawer" aria-label="close sidebar" className="drawer-overlay"></label>
+            <ul className="menu p-0 w-[210px] min-h-full bg-[#111827] text-base-content">
+              {/* Sidebar content here */}
+              <Sidebar />
+            </ul>
+          </div>
+        </div>
+      </div>
+  )
+}
