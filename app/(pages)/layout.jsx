28fa579421200a93629d32995b2e608ef816b961
Terry Babyuk <tbabyuk@gmail.com>
2024-06-12 22:28:09 -0400

    deploy attempt 3

--- app/(pages)/layout.jsx
+++ app/(pages)/layout.jsx
@@ -1,18 +1,16 @@
-// import { Navbar } from "../components/Navbar";
-// import { Sidebar } from "../components/Sidebar";
+import { Navbar } from "../components/Navbar";
+import { Sidebar } from "../components/Sidebar";
 
-import { Topnav } from "../components/Topnav";
-import { Sidenav } from "../components/Sidenav";
 
 export default function WorksheetsLayout({ children }) {
     
     return (
         <>
             <div className="hidden md:flex md:flex-col md:w-[210px] md:fixed md:inset-y-0 bg-gray-900">
-                <Sidenav />
+                <Sidebar />
             </div>
             <div className="md:ml-[210px]">
-                <Topnav />
+                <Navbar />
                 <main>
                     {children}
                 </main>
