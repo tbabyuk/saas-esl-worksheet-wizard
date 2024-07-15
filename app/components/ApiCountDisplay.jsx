01e89c98e96461ca1dc64ea2530d6bd1868ce6cd
Terry Babyuk <tbabyuk@gmail.com>
2024-07-15 16:08:03 -0400

    updated API Count fetch logic in ApiCountDispay component

--- app/components/ApiCountDisplay.jsx
+++ app/components/ApiCountDisplay.jsx
@@ -1,33 +1,39 @@
+"use client"
+
 import { connectToESLWorksheetWizardDB } from "@/db/database";
 import { User } from "@/models/models";
 import { auth } from '@clerk/nextjs/server';
+import { useState, useEffect } from "react";
 
-export const dynamic = 'force-dynamic'
-
-
-const getApiCount = async () => {
-
-    const {userId} = auth();
-
-
-    console.log("Logging userId from ApiCountDisplay:", userId)
-
-    try {
-        await connectToESLWorksheetWizardDB();
-        const user = await User.findOne({ userClerkId: userId });
-        console.log("logging user from ApiCountDisplay:", user)
-        return user.userApiCount;
-    } catch (error) {
-        console.log("An error occured:", error)
-    }
-}
 
 
 export const ApiCountDisplay = async () => {
 
-    const count = await getApiCount()
+    const [apiCount, setApiCount] = useState("")
+    
+    const {userId} = auth();
+
+    console.log("Logging userId from ApiCountDisplay:", userId)
+
+
+    const fetchAPICount = async () => {
+        try {
+            await connectToESLWorksheetWizardDB();
+            const user = await User.findOne({ userClerkId: userId });
+            console.log("logging user from ApiCountDisplay:", user)
+            setApiCount(user.userApiCount);
+            } catch (error) {
+                console.log("An error occured:", error)
+            }    
+        }
+
+
+    useEffect(() => {
+        fetchAPICount()
+    }, [])
+
 
     return (
-        <p className="text-lg text-center py-2 bg-emerald-500/10">Your credit balance: &nbsp;&nbsp; <span className="font-bold text-gray-600">{count} {count === 1 ? "credit" : "credits"}</span></p>
+        <p className="text-lg text-center py-2 bg-emerald-500/10">Your credit balance: &nbsp;&nbsp; <span className="font-bold text-gray-600">{apiCount && apiCount} {apiCount && apiCount === 1 ? "credit" : "credits"}</span></p>
     )
   }
